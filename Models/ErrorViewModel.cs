2c5b1f031d913cbe48acd58a689a803d3a7c6cb3
BOBY <BOBY@DESKTOP-LRADAAA>
2021-01-27 12:05:16 -0800

    STABLE

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SHOP_DECOMPILED.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
